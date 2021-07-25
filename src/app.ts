3135a80d25680739962dc0e43db26982bb4140c9
sambuite <murilosambuite@gmail.com>
2021-07-25 11:41:22 -0300

    Added swagger-ui-express

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,15 @@
 import express from 'express';
 import { router } from './routes';
 import cors from 'cors';
+import swaggerUi from 'swagger-ui-express';
+
+import swaggerDocs from '@config/swagger.json';
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs));
 app.use(router);
 
 export { app };
